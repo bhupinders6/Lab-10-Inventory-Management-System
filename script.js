b19712e461073ee00dc5c56134ede1d04b5eccd4
SBhupinder <bsingh168@hotmail.com>
2026-03-25 19:29:21 -0400

    Added two perishable products

--- script.js
+++ script.js
@@ -24,3 +24,7 @@ class PerishableProduct extends Product {
         return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}`;
     }
 }
+
+const milk = new PerishableProduct("Milk", 1.50, 10, "2026-04-31");
+const yogurt = new PerishableProduct("Yogurt", 3.00, 15, "2026-05-31");
+
